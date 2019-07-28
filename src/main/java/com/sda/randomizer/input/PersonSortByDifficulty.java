182385d35c25275693aa25ec7b3fdf3e4dc346de
cosmin_bucur <cosmin.bucur@orange.com>
2019-07-28 15:54:14 +0300

    fix and review raluca's commit

--- src/main/java/com/sda/randomizer/input/PersonSortByDifficulty.java
+++ src/main/java/com/sda/randomizer/input/PersonSortByDifficulty.java
@@ -1,5 +1,7 @@
 package com.sda.randomizer.input;
 
+import com.sda.randomizer.Person;
+
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -9,8 +11,7 @@ public class PersonSortByDifficulty {
     List<Person> persons = new ArrayList<>();
 
     public List<Person> sortByDifficultySetting(List<Person> persons) {
-
         persons.sort(Comparator.comparingInt(Person::getDifficulty));
-
+        return persons;
     }
 }
