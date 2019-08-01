8330b9f0fe630f8bc36849a223f76a02f5f904f0
DanNistor1 <dan.nistor1@yahoo.ro>
2019-08-01 19:23:06 +0300

    Completed repository

--- src/main/java/com/sda/randomizer/PersonRepository.java
+++ src/main/java/com/sda/randomizer/PersonRepository.java
@@ -3,16 +3,48 @@ package com.sda.randomizer;
 import java.util.List;
 
 public class PersonRepository {
-    public List<Person> removePerson(List<Person> people, Person person) {
-        people.remove(person);
-        return people;
+
+    private List<Person> list;
+
+    public PersonRepository(List<Person> list) {
+        this.list = list;
     }
 
-    public List<Person> updatePerson(List<Person> people, Person person, String name, int difficulty) {
-        int personToUpdatepeople = people.indexOf(person);
-        person.setName(name);
-        person.setDifficulty(difficulty);
-        people.set(personToUpdatepeople, person);
-        return people;
+/*
+    public Person selectStandard(String name) {
+        Person person = null;
+        for (Person p : list) {
+            if (p.getName().equals(name)) {
+                person = p;
+                break;
+            }
+        }
+        return person;
+    }
+*/
+
+    public Person select(String name) {
+        Person person = list.stream()
+                .filter(p -> p.getName().equals(name))
+                .findFirst()
+                .get();
+        return person;
+    }
+
+    public void remove(Person person) {
+        list.remove(person);
+    }
+
+    public void update(Person person, String newName, int newDifficulty) {
+        person.setName(newName);
+        person.setDifficulty(newDifficulty);
+    }
+
+    public void update(Person person, String newName) {
+        person.setName(newName);
+    }
+
+    public void update(Person person, int newDifficulty) {
+        person.setDifficulty(newDifficulty);
     }
 }
