df66dd0eafcfcf0901154369d162f44e8e2544d4
cosmin_bucur <cosmin.bucur@orange.com>
2019-08-03 14:46:09 +0300

    code reivew

--- src/main/java/com/sda/collections/WriteInFile.java
+++ src/main/java/com/sda/collections/WriteInFile.java
@@ -1,4 +1,4 @@
-package com.sda.StreamsAndCollections;
+package com.sda.collections;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -9,7 +9,7 @@ import java.util.List;
 public class WriteInFile {
 
     public void writeInFile(List<String> list) throws IOException {
-        Path path = Paths.get("C:\\Users\\Alexandrul46\\IdeaProjects\\SDA2\\sda-demo\\src\\main\\resources\\firstNamesFile");
+        Path path = Paths.get("C:\\dev\\rares\\sda-demo\\src\\main\\resources\\people-output.txt");
         Files.write(path, list);
     }
 
